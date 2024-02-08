8ffd70540b856ca2c0a5807853cdfd81b0174503
Chris O'Malley <Chris.O'Malley@concepts.co.nz>
2024-02-09 10:47:34 +1300

    Add chat sidebar to HTML output

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,10 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+html_sidebars = {
+    '**': ['chat.html']
+}
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
